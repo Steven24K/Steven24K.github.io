9063d8ba6914f1d45f1b85d57693cec726536fa2
Steven <s.koerts2@gmail.com>
2017-12-28 13:28:09 +0100

    Setup react + bootstrap

---
+++ src/index.tsx
@@ -0,0 +1,7 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+
+ReactDOM.render(
+    <div className="jumbotron"><h1>This website is under development</h1></div>,
+    document.getElementById("react-content")
+);
